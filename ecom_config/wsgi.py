43ec820c00acb2911f84c1abeec9863b1655a3cd
vaibhav20012811 <vaibhavchamkur143@gmail.com>
2024-11-01 13:25:08 +0530

    gunicorn ecom_config.wsgi:application

--- ecom_config/wsgi.py
+++ ecom_config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gunicorn ecom_config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gunicorn ecom_config.wsgi:application")
 
 application = get_wsgi_application()
